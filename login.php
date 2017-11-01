1216c7e1b17323280dc8512b9d62471acad1ab08
Laantje <laan.j.david@gmail.com>
2017-11-01 12:31:58 +0100

    Created register.php and login.php but its still empty

---
+++ login.php
@@ -0,0 +1,9 @@
+<HTML>
+	<HEAD>
+		<TITLE>Website Titel</TITLE>
+	</HEAD>
+	<BODY BGCOLOR="FFFFFF">
+		<h1 align="center">Login</h1>
+
+	</BODY>
+</HTML>
---
+++ register.php
@@ -0,0 +1,9 @@
+<HTML>
+	<HEAD>
+		<TITLE>Website Titel</TITLE>
+	</HEAD>
+	<BODY BGCOLOR="FFFFFF">
+		<h1 align="center">Register</h1>
+
+	</BODY>
+</HTML>
