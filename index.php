e97205bd312157fe2ed43f1d6e1d32903d00e1b3
Laantje <laan.j.david@gmail.com>
2017-11-02 13:56:45 +0100

    Added database SQL

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <HTML>
 	<HEAD>
-		<TITLE>Website Titel</TITLE>
+		<TITLE>Wascessoires</TITLE>
+		<h1 align="center">Wascessoires</h1>
+		<h3 align="center">Alles voor de was</h3>
+		<TABLE ALIGN="center" BORDER="2" WIDTH="50%" CELLPADDING="4">
+			<TR>
+				<TH>Home</TH><TH>Winkel</TH><TH>Over ons</TH>
+			</TR>
+		</TABLE>
 	</HEAD>
 	<BODY BGCOLOR="FFFFFF">
-		<h1 align="center">Hallo</h1>
-		<h2 align="center">Test</h2>
+		<h1 align="left">Welkom</h1>
 	</BODY>
 </HTML>
