0d7cee4b396078c42f31a19693959186e85ad0a7
sanvander <33283794+sanvander@users.noreply.github.com>
2017-11-03 11:47:33 +0100

    Add files via upload

---
+++ toolbar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+
+<!-- hiermee roepen we het stijlblad op -->
+<link href="stijlblad1.css" rel="stylesheet" type="text/css"/> 
+    <!-- hier starten de divs, de stijl van de divs wordt aangegeven op het stijlblad -->
+    <div id="header">
+        <div id="homeknop">
+            </br><a href="homepagina.php"><img src="home.png" height="30px"></a>
+        </div>
+            <div id="topmenu">
+                    </br>
+                <!-- dit zijn de icons voor de knoppen in de toolbar -->
+                <a href="#"><img src="menu.png" height="30px"></a>&nbsp&nbsp
+                <a href="inlogpagina.php"><img src="login.png" height="30px"></a>&nbsp&nbsp
+                <a href="#"><img src="cart2.png" height="30px"></a>
+                <!-- dit is het logo -->
+                
+            </div>
+            <div id="logo"><img src="logo.png" align="middle"></div>
+    </div>
+    <!-- dit stukje code maakt een lijn, waar de header stopt en waar de content van de site begint -->
+<hr color="#000000">
+
+</HTML>
