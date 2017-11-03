1f86d101355c760c224fcee5a7c7e4fea1a6506c
Paardenxx <33283395+Paardenxx@users.noreply.github.com>
2017-11-03 12:43:02 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <title> Project </title>
+    <link href="stijlblad.css" rel="stylesheet" type="text/css"/>
+<div id="container">
+    <div id="header">Dit is de header
+        <div id="topmenu">
+            <a href="#">Inloggen</a>
+            <a href="#">Registreren</a>
+            <a href="#">Winkelmandje</a>
+        </div>
+        <span class="title">Wascessoires</span>
+    </div>
+    <!-- bij de content komt informatie over de site -->
+    <div id="content"><center><h1>Welkom op de website Wascessoires!</h1></center>
+        <div id="block1"><h2>Informatie</h2>
+            <p>Wascessoires is opgericht in 2017 door 5 jongens die voor school een 
+            project moesten maken. Het idee om een wasknijper webshop te maken heeft een tijdje geduurd. 
+            We hebben verschillende plannen voor onze neus gehad, en zo hebben we uiteindelijk voor de meest originele gekozen.</p>
+            <p>Over heel de wereld worden wasknijpers gebruikt. Iedereen heeft was dat gedroogd moet worden. 
+                Dit gebeurt door de mens en waar mensen aan werken wordt er wel eens een foutje gemaakt.
+                Het kan dus zo gebeuren dat er een keer een wasknijper verloren gaat of kapot gaat. 
+                Al met al wasknijpers ben je altijd nodig en daarom leek het ons geschikt om hier een webshop van te maken.</p>             
+            </div>
+        <div id="block2"><center><h2>Contactinformatie</h2></center>
+                <ul>
+                    <li>Projectleider: Tjesse</li>
+                    <li>Telefoonnummer: 06 - 12345678</li>
+                    <li>Adres: Zernikeplein 11</li>
+                </ul>
+            </p>       
+        </div>
+        <div id="block2">
+            <a href="testinloggen.php"><img src="meldjeaan.jpg"></a>
+        </div>
+    </div>   
+    <!-- Dit is de footer-->
+    <div id="onderkant">
+        <div id="block"><h3>Account aanmaken</h3>
+            Door rechtsboven op registreren te klikken is het mogelijk om een account aan te maken.</div>
+        <div id="block"><h3>Inloggen</h3>
+            Door rechtsboven op inloggen te klikken is het mogelijk om met jouw gegevens in te loggen.</div>
+        <div id="block"><h3>Bestelling plaatsen</h3>
+            Nu je ingelogd bent is het mogelijk om een bestelling te plaatsen, dit is mogelijk door het menu linksboven.</div>
+    </div>
+</div>
